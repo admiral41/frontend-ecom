49028720a899d638e915ea278069bf835f541b0a
Susan Kafle <kafle.susan671@outlook.com>
2025-06-12 17:58:37 +0545

    fix

--- app/all-products/page.jsx
+++ app/all-products/page.jsx
@@ -1,6 +1,5 @@
 'use client'
 import ProductCard from "@/components/ProductCard";
-import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { useAppContext } from "@/context/AppContext";
 
@@ -10,7 +9,6 @@ const AllProducts = () => {
 
     return (
         <>
-            <Navbar />
             <div className="flex flex-col items-start px-6 md:px-16 lg:px-32">
                 <div className="flex flex-col items-end pt-12">
                     <p className="text-2xl font-medium">All products</p>
